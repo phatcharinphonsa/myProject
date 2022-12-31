800960de7f7c3b9c27372daf83b64b2f8023bc92
Kokiat <ccubisoft@gmail.com>
2022-12-31 21:24:58 +0700

    first commit

---
+++ resources/views/ifinn/client/verify.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/contentLayoutMaster') // choose any one option {contentLayoutMaster/fullLayoutMaster}
+
+@section('title', 'ยืนยันตัวตน')  // change title accrodingly
+
+@section('content')
+  <div>
+      <h1>ทักครับ</h1>
+  </div>
+@endsection
